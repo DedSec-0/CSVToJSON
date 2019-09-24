149e409df400cadc84f9f7c6e3be6cefda807b3d
Hamza Awan <hamza_awan1996@hotmail.com>
2019-09-24 23:49:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const csvFilePath='./customer-data.csv'
-const csv=require('csvtojson')
+const csvFilePath = './customer-data.csv'
+const csv = require('csvtojson')
+const fs = require('fs')
 
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj) => {
-    console.log(jsonObj[0]);
+    fs.writeFileSync('customer-data.json', JSON.stringify(jsonObj, null, 2), (error) => {
+        if (error){
+            throw 'Error!' + error
+        }
+    })
+
+    console.log('Done!');
+})
+.catch((error) => {
+    console.log(error);
 })
